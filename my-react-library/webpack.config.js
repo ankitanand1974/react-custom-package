fb7ff03bb19ce4b4349fd0c5d434884db65b7fc3
Ankit Anand <ankitanand1974@gmail.com>
2024-02-11 19:45:27 +0530

    first commit

---
+++ my-react-library/webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'my-react-library',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        },
+      },
+    ],
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Button } from 'my-react-library'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My First React App</h1>
+      <p>Click the button below:</p>
+      <Button onClick={() => console.log('Button clicked')}>Click me</Button> {/* Using the Button component */}
     </div>
   );
 }
